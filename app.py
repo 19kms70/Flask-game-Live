df02227a360f2b4fb85c3fdf654d5e1e7ee0f87c
19.kms.70 <19.kms.70@gmail.com>
2022-08-09 16:26:04 +0300

    add all file

---
+++ app.py
@@ -0,0 +1,41 @@
+﻿from flask import Flask, render_template, request, redirect
+from game_of_life import *
+
+app = Flask(__name__)
+
+
+class Game(metaclass=SingletonMeta):
+    def __init__(self, text="test"):
+        self.text = text
+
+
+@app.route('/', methods=["POST", "GET"])
+def index():
+    if request.method == "POST":
+        width = request.form.get("width")
+        height = request.form.get("height")
+        print(width, ' ', height)
+        if not width:
+            width = 25
+        if not height:
+            height = 25
+
+        GameOfLife(int(width), int(height))
+        return redirect("/live")
+    else:
+        GameOfLife(25, 25)
+
+    return render_template('index.html')
+
+
+@app.route('/live')
+def live():
+    live = GameOfLife()
+    if live.counter > 0:
+        live.form_new_generation()
+    live.counter += 1
+    return render_template('live.html', live=live)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
